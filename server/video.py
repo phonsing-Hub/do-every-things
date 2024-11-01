d9b4b7b156570f66e4d8c8e214ae42067b971d39
p.phonsing_ <p.phonsing.tal@gmail.com>
2024-11-01 21:25:32 +0700

    0.1.0

--- server/video.py
+++ server/video.py
@@ -2,6 +2,6 @@ from src.lib.Face_Streaming import Stream
 
 video = Stream()
 # video.readImg_encoding("src/images/me", "NOAH")
-# video.show_results()
+video.show_results()
 video.get_data_encoding_db()
 video.open()
